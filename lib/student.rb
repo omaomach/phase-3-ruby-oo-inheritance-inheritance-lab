06886f9a5bb34be996ee544c03ca66a999d019b4
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-26 01:37:27 +0300

    completed phase 3 ruby oo inheritance lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn knows
+        @knowledge << knows
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
